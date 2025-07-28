707b81a93e4c7dbab724c06f35b782cee4c7a30e
GURUMOORTHY SUBBAIAH <126964208+GURUMOORTHYSUBBAIAH@users.noreply.github.com>
2025-07-28 21:42:03 +0530

    Update auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://your-backend.onrender.com/api/auth';
+const API_URL = 'https://expense-tracker-68qa.onrender.com/api/auth';
 
 const register = async (userData) => {
   const response = await axios.post(`${API_URL}/register`, userData);
